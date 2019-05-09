360b7f2365413888190b3333ec4096bb9fda53c4
hochan222 <201502766@hufs.ac.kr>
2019-05-09 18:38:55 +0900

    part 1 complete

---
+++ frontend/playground/src/stories/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { storiesOf, addDecorator } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import backgrounds from '@storybook/addon-backgrounds';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Input', module)
+  .addDecorator((story) => <div style={{ margin: '50px'}} >{story()}</div>)
+  .addDecorator(backgrounds([
+    { name: "white", value: "#ffffff", default: true },
+    { name: "pink", value: "#ff00ff" },
+    { name: "black", value: "#000000" },
+  ]))
+  .add('default', () => <Input onChange={action('changed')}/>)
+  .add('disabled', () => <Input disabled/>)

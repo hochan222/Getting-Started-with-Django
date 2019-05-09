a75bfaee6d08ebf1d03f5be502d5a70cb6d50f7f
hochan222 <201502766@hufs.ac.kr>
2019-05-09 09:54:44 +0900

    add polls

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>hi</p>
+    </div>
+  );
+}
+
+export default App;
